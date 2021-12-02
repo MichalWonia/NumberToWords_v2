0134bd414a044972a5161d04487ace5073a8ce14
Michal Wonia <spodek.mw@gmail.com>
2021-12-02 15:00:42 +0100

    The enterTheNumber method has been added, which allows you to specify a number to be converted to words.

--- src/NumberToWords_v2.java
+++ src/NumberToWords_v2.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class NumberToWords_v2 {
 
-    public static void main(String[] args) {
+    public static int enterTheNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number in the range -1000 to 1000:");
+        int number;
 
+        while (true) {
+            if (sc.hasNextInt()) {
+                number = sc.nextInt();
+                if (number > 1000 || number < -1000) {
+                    System.out.println("Number out of range. Specify a number in range -1000 to 1000");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("You must enter the number");
+                sc.next();
+            }
+        }
+        sc.close();
+        return number;
     }
 
+
+    public static void main(String[] args) {
+        enterTheNumber();
+
+
+    }
 }
 
