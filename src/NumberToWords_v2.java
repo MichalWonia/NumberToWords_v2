0a1b1003d962672889902ee890e45fb71ddcf589
Michal Wonia <spodek.mw@gmail.com>
2021-12-02 15:18:59 +0100

    The numberToWords method has been added to convert number into words. Creating variables that store numbers in words and transforming the given number into an array of digits.

--- src/NumberToWords_v2.java
+++ src/NumberToWords_v2.java
@@ -24,10 +24,76 @@ public class NumberToWords_v2 {
         return number;
     }
 
+    public static void numberToWords(int number)
+    {
+        String thousands = "thousand";
+        String hundreds = "hundred ";
+
+        String[] tens = new String[9];
+        tens[0] = "ten ";
+        tens[1] = "twenty ";
+        tens[2] = "thirty ";
+        tens[3] = "forty ";
+        tens[4] = "fifty ";
+        tens[5] = "sixty ";
+        tens[6] = "seventy ";
+        tens[7] = "eighty ";
+        tens[8] = "ninety ";
+
+        String[] teens = new String[9];
+        teens[0] = "eleven";
+        teens[1] = "twelve";
+        teens[2] = "thirteen";
+        teens[3] = "fourteen";
+        teens[4] = "fifteen";
+        teens[5] = "sixteen";
+        teens[6] = "seventeen";
+        teens[7] = "eighteen";
+        teens[8] = "nineteen";
+
+        String[] unities = new String[10];
+        unities[0] = "zero";
+        unities[1] = "one";
+        unities[2] = "two";
+        unities[3] = "three";
+        unities[4] = "four";
+        unities[5] = "five";
+        unities[6] = "six";
+        unities[7] = "seven";
+        unities[8] = "eight";
+        unities[9] = "nine";
+
+        System.out.println("Your number in words is:");
+
+        if(number < 0)
+        {
+            System.out.print("Minus ");
+            number = -number;
+        }
+
+        String numberString = Integer.toString(number);
+        int[] numArray = new int[4];
+        int end = numberString.length() - 1;
+
+        for(int i = 0; i < numberString.length(); i++)
+        {
+            numArray[i] = Character.getNumericValue(numberString.charAt(end));
+            end--;
+        }
+
+        int hun = numArray[2];
+        int ten = numArray[1];
+        int uni = numArray[0];
+
+        System.out.println(hun);
+        System.out.println(ten);
+        System.out.println(uni);
+
+    }
 
     public static void main(String[] args) {
-        enterTheNumber();
-
+        int number = enterTheNumber();
+        numberToWords(number);
 
     }
 }
